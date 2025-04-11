a26c8dea3247c829bc631f520d88e0c42e4e2f3a
martinabolen <martina.bolen@gmail.com>
2025-04-11 09:13:05 -0400

    m

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,13 +1,13 @@
 router.get('/', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
+        const db = await connectToDatabase();
 
         // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
+        const collection = db.collection("gifts");
 
         // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
-        // const gifts = {{insert code here}}
+        const gift = await collection.findOne({ id: id });  
 
         // Task 4: return the gifts using the res.json method
         res.json(/* {{insert code here}} */);
